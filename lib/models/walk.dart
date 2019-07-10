6d0bc86bedf1c4cf4409b912fbda4f0149d0aa0d
Dominic Järmann <dominic.jaermann@icloud.com>
2019-07-10 11:11:27 +0200

    Fixed some erros

--- lib/models/walk.dart
+++ lib/models/walk.dart
@@ -1,8 +1,16 @@
 class Walk {
   int duration;
   Walk({this.duration});
+  bool hasDuration(){
+    return this.duration != null;
+  }
   factory Walk.fromJson(Map<String, dynamic> json) {
-    if (json == null) return null;
+    if (json == null) {
+      print("Tried to initializie walk with json null");
+      return null;
+    }else{
+      print("json is: $json");
+    }
     return new Walk(
       duration: json['duration'],
     );
